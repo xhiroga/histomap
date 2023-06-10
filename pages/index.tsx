974256936737d6fd7c9f6344dc7363a91a25dee8
xhiroga <xhiroga@users.noreply.github.com>
2023-06-11 06:56:16 +0900

    chore: load json from local

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,62 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { GetServerSideProps } from 'next';
+// import Papa from 'papaparse';
+import { geoJson } from '../utils/sample-data';
+import Layout from '../components/Layout';
+import Link from 'next/link';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+interface Feature {
+  type: string;
+  geometry: {
+    type: string;
+    coordinates: [number, number];
+  };
+  properties: {
+    name: string;
+    year: string;
+  };
+}
 
-export default IndexPage
+// const getGeoJsonFromSpreadsheet = async () => {
+//   const spreadsheetUrl = 'YOUR_SPREADSHEET_PUBLISHED_CSV_URL';
+
+//   const response = await fetch(spreadsheetUrl);
+//   const csvData = await response.text();
+
+//   const data = Papa.parse(csvData, { header: true }).data;
+
+//   // GeoJSON形式にデータを変換
+//   const geoJson: Feature[] = data.map((item: any) => ({
+//     type: 'Feature',
+//     geometry: {
+//       type: 'Point',
+//       coordinates: [Number(item.longitude), Number(item.latitude)],
+//     },
+//     properties: {
+//       name: item.name,
+//       year: item.year,
+//     },
+//   }));
+
+//   return geoJson;
+// }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      geoJson,
+    },
+  };
+};
+
+const Home = ({ geoJson }: { geoJson: Feature[] }) => {
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">About</Link>
+      </p>
+    </Layout>
+  );
+};
+
+export default Home;
--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -7,3 +7,39 @@ export const sampleUserData: User[] = [
   { id: 103, name: 'Caroline' },
   { id: 104, name: 'Dave' },
 ]
+
+export const geoJson = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ガンディーの誕生",
+        "year": 1869,
+        "image": "https://upload.wikimedia.org/wikipedia/commons/8/8f/Gandhi_and_Laxmidas_2.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          70.794922,
+          21.641706
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ロンドン法科大学への入学",
+        "year": 1888,
+        "image": "https://upload.wikimedia.org/wikipedia/commons/d/dc/Gandhi_student.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.116600,
+          51.523767
+        ]
+      }
+    },
+  ]
+}
