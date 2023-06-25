6062b5919d84ff940ac84357e63d54accff717e4
xhiroga <xhiroga@users.noreply.github.com>
2023-06-25 11:13:07 +0900

    fix: adjust dot and fukidashi

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -13,15 +13,14 @@ interface MapComponentProps {
 const pointToLayer = (feature, latlng) => {
   const dotSize = 0.5;
   const fukidashiWidth = 9;
-  const fukidashiPadding = 0.5;
 
   const customIcon = new L.DivIcon({
     // TODO: Tailwind CSSを削除したことで、リセットCSSがなくテキストに余分なmargin-topが設定されている。
     html: `
-    <div style="background-color: #FF9900; border-radius: 50%; width: ${dotSize}rem; height: ${dotSize}rem;"></div>
-    <div style="align-items:center; display: flex; flex-direction: column; width: fit-content; margin-left: ${-(fukidashiWidth + fukidashiPadding) / 2}rem;">
+    <div style="background-color: #FF9900; border-radius: 50%; width: ${dotSize}rem; height: ${dotSize}rem; margin-left: ${-dotSize / 2}rem;"></div>
+    <div style="align-items:center; display: flex; flex-direction: column; width: fit-content; margin-left: ${-fukidashiWidth / 2}rem;">
       <div style="margin-top: -12px; border: 12px solid transparent; border-bottom: 10px solid #fff;"></div>
-      <div style="background-color: white; border-radius: 0.5rem; padding: ${fukidashiPadding}rem; box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06); width: ${fukidashiWidth}rem;">
+      <div style="background-color: white; border-radius: 0.5rem; padding: 0.5rem; box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06); width: ${fukidashiWidth}rem;">
         <div style="font-weight: bold; font-size: 1rem; margin-bottom: 0.25rem; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${feature.properties.name}</div>
         <div style="font-size: 0.875rem; margin-bottom: 0.25rem; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${feature.properties.edtf}</div>
       </div>
