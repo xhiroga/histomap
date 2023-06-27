0d5e9a4cf6333a4884837eb0bd43014b33a75050
xhiroga <xhiroga@users.noreply.github.com>
2023-06-27 09:02:47 +0900

    chore: local path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ja'],
+    localePath: path.resolve('./public/static/locales')
   },
 }
